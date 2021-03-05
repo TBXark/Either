e7e78c8d427b29a66e7eb383b03a31d72d842a3f
TBXark <tbxark@outlook.com>
2021-03-05 18:07:21 +0800

    Add test case

--- Tests/EitherTests/EitherTests.swift
+++ Tests/EitherTests/EitherTests.swift
@@ -2,14 +2,31 @@ import XCTest
 @testable import Either
 
 final class EitherTests: XCTestCase {
-    func testExample() throws {
+    func testEqual() throws {
         let d1 = Either2<String, Int>.a("Hello world")
         let raw = try JSONEncoder().encode(d1)
         let d2 = try JSONDecoder().decode(Either2<String, Int>.self, from: raw)
-        XCTAssertEqual(d1, d2, "Pass")
+        XCTAssertEqual(d1, d2)
+    }
+    
+    func testNotEqual() throws {
+        let d1 = Either2<Int, Int>.a(123)
+        let d2 = Either2<Int, Int>.b(123)
+        XCTAssertTrue(d1 != d2)
+    }
+    
+    func test6Equal() throws {
+        let d1 = Either6<String, Int, Float,  [String], [Int], [String: Int]>.a("Hello world")
+        let raw = try JSONEncoder().encode(d1)
+        let d2 = try JSONDecoder().decode(Either6<String, Int, Float,  [String], [Int], [String: Int]>.self, from: raw)
+        XCTAssertEqual(d1, d2)
     }
 
+
     static var allTests = [
-        ("testExample", testExample),
+        ("testEqual", testEqual),
+        ("testNotEqual", testNotEqual),
+        ("test6Equal", test6Equal),
+
     ]
 }
